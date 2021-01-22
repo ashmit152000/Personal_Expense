ef6b7212f25cbf091e9023ccbc87f2c9a445a214
Ashmit Raj <ashmitraj@Ashmits-MacBook-Air.local>
2021-01-22 16:15:52 +0530

    added chart related data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:Expenses_App/widgets/chart.dart';
 import 'package:flutter/material.dart';
 
 import './widgets/new_transaction.dart';
@@ -17,12 +18,12 @@ class MyApp extends StatelessWidget {
         accentColor: Colors.amber,
         fontFamily: 'Quicksand',
         textTheme: ThemeData.light().textTheme.copyWith(
-          headline6: TextStyle(
-              fontFamily: 'OpenSans',
-              fontWeight: FontWeight.w700,
-              color: Colors.black,
-              fontSize: 18),
-        ),
+              headline6: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                  fontSize: 18),
+            ),
         appBarTheme: AppBarTheme(
           textTheme: ThemeData.light().textTheme.copyWith(
               headline6: TextStyle(
@@ -58,6 +59,12 @@ class _MyHomePageState extends State<MyHomePage> {
     // ),
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
+
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
       title: txTitle,
@@ -86,12 +93,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
         title: Text('Expense App'),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.add),  
+            icon: Icon(Icons.add),
             onPressed: () => _startAddNewTransaction(context),
           ),
         ],
@@ -101,14 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,4 +1,5 @@
 import 'package:Expenses_App/models/transaction.dart';
+import 'package:Expenses_App/widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -7,7 +8,7 @@ class Chart extends StatelessWidget {
 
   Chart(this.recentTransactions);
 
-  List<Map<String, Object>> get groupedTransactions {
+  List<Map<String, Object>> get groupedTransactionsValues {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(
         Duration(days: index),
@@ -22,16 +23,42 @@ class Chart extends StatelessWidget {
       }
       print(weekDay);
       print(totalSum);
-      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 2),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get maxSpending {
+    return groupedTransactionsValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    print(groupedTransactionsValues);
     return Card(
-      elevation: 6,
-      margin: EdgeInsets.all(20),
-      child: Row(children: []),
-    );
+        elevation: 6,
+        margin: EdgeInsets.all(20),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              ...groupedTransactionsValues.map((data) {
+                return Flexible(
+                    fit: FlexFit.tight,
+                    child: ChartBar(
+                        data['day'],
+                        data['amount'],
+                        maxSpending == 0.0
+                            ? 0.0
+                            : (data['amount'] as double) / maxSpending));
+              }).toList()
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          ),
+        ));
   }
 }
