e24b78920e0c18723744f5a5bf5c484b0b55acb7
Ashmit Raj <ashmitraj@Ashmits-MacBook-Air.local>
2021-01-28 08:21:48 +0530

    new push

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -9,19 +9,20 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return LayoutBuilder(builder:(ctx, constraints){
+      return Column(
       children: [
         Container(
-          height: 20,
+          height: constraints.maxHeight * 0.15,
           child: FittedBox(
             child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
           ),
         ),
         SizedBox(
-          height: 4,
+          height: constraints.maxHeight * 0.05,
         ),
         Container(
-          height: 60,
+          height: constraints.maxHeight * 0.6 ,
           width: 10,
           child: Stack(
             children: [
@@ -43,10 +44,11 @@ class ChartBar extends StatelessWidget {
           ),
         ),
         SizedBox(
-          height: 4,
+          height: constraints.maxHeight * 0.05,
         ),
-        Text(label)
+        Container(child: FittedBox(child: Text(label)), height: constraints.maxHeight * 0.15,)
       ],
     );
+    },); 
   }
 }
